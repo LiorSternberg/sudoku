41c59ee1c4dcb235d98b7e99921517bfa0272af6
Lior Sternberg <liors1@mail.tau.ac.il>
2019-08-04 13:41:41 +0300

    Created the basic linked-list of game states (will need to review later on)

--- StatesList.h
+++ StatesList.h
@@ -1,4 +1,17 @@
 #ifndef FINAL_PROJECT_STATESLIST_H
 #define FINAL_PROJECT_STATESLIST_H
 
+#include "Board.h"
+
+typedef struct State_ {
+    Board* board;
+    struct State_* next;
+    struct State_* prev;
+} State;
+
+
+typedef struct {
+    State* head;
+} StatesList;
+
 #endif
