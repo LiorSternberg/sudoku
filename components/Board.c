d27d51d11cda997a9c905fc78268280d6a25725c
Tslil Chen <tslilchen@mail.tau.ac.il>
2019-08-06 12:07:09 +0300

    Added - structs: game_cell and Board. Functions: init_empty_board, destroy_board, validate_memory_allocation and report_memory_allocation_error.

--- components/Board.c
+++ components/Board.c
@@ -1,1 +1,38 @@
+#include <stdlib.h>
+#include <libintl.h>
 #include "Board.h"
+
+void report_memory_allocation_error(char* function_name) {
+    printf("Error: function \"%s\" has failed\n", function_name);
+    exit(1);
+}
+
+void validate_memory_allocation(char* function_name, void *p) {
+    if (p == NULL) {
+        report_memory_allocation_error(function_name);
+    }
+}
+
+void init_empty_board(Board *board, int dim, int rows_in_block, int columns_in_block){
+    int i, j;
+
+    board->num_of_rows_in_block = rows_in_block;
+    board->num_of_columns_in_block = columns_in_block;
+    board->dim = dim;
+
+    board->cells_arr = calloc(dim*dim, sizeof(game_cell));
+    validate_memory_allocation("init_empty_board", board->cells_arr);
+
+    for (i=0; i < dim; i++) {
+        for (j=0; j < dim; j++) {
+            board->cells_arr[i][j].val = CLEAR;
+            board->cells_arr[i][j].fixed = false;
+            board->cells_arr[i][j].erroneous = false;
+        }
+    }
+
+}
+
+void destroy_board(Board *board){
+    free(board->cells_arr);
+}
--- components/Board.h
+++ components/Board.h
@@ -1,9 +1,36 @@
 #ifndef FINAL_PROJECT_BOARD_H
 #define FINAL_PROJECT_BOARD_H
 
+#include <stdbool.h>
+
+#define CLEAR (0)
+
 typedef struct {
- /* TODO */
+    int val;
+    bool fixed;
+    bool erroneous;
+} game_cell;
+
+typedef struct {
+    int dim;
+    int num_of_rows_in_block;
+    int num_of_columns_in_block;
+    game_cell cells_arr[][];
+    bool solved;
 } Board;
+
+
+/* Alert of a failed memory allocation, and exit */
+void report_memory_allocation_error(char* function_name);
+
+/* Validate that a memory allocation was successful. If not, report failure */
+void validate_memory_allocation(char* function_name, void *p);
+
+/* Initialize the given board:
+ * set the dimension fields.
+ * set the cells values to CLEAR, which means that the board is empty.
+ * */
+void init_empty_board(Board *board, int dim, int rows_in_block, int columns_in_block);
 
 void destroy_board(Board *board);
 
