f977bd1940d49e7a14e1b2a071e79d87ac2ba06e
Lior Sternberg <liors1@mail.tau.ac.il>
2019-08-06 13:32:37 +0300

    Refactored Board & GameCell to match the convention of the other components (each component defines a "create" and "destroy" function, and each component is *solely* responsible for releasing the resources it allocated).

--- components/Board.c
+++ components/Board.c
@@ -1,38 +1,73 @@
 #include <stdlib.h>
 #include <libintl.h>
 #include "Board.h"
+#include "../MemoryError.h"
 
-void report_memory_allocation_error(char* function_name) {
-    printf("Error: function \"%s\" has failed\n", function_name);
-    exit(1);
+
+BoardCell* create_cell() {
+    BoardCell *cell = malloc(sizeof(BoardCell));
+    validate_memory_allocation("create_cell", cell);
+
+    cell->val = CLEAR;
+    cell->fixed = false;
+    cell->erroneous = false;
+
+    return cell;
 }
 
-void validate_memory_allocation(char* function_name, void *p) {
-    if (p == NULL) {
-        report_memory_allocation_error(function_name);
+void destroy_cell(BoardCell *cell) {
+    free(cell);
+}
+
+BoardCell** create_row(int length) {
+    int i;
+    BoardCell **row = malloc(length * sizeof(BoardCell*));
+    validate_memory_allocation("create_row", row);
+
+    for (i=0; i < length; i++) {
+        row[i] = create_cell();
     }
+
+    return row;
 }
 
-void init_empty_board(Board *board, int dim, int rows_in_block, int columns_in_block){
-    int i, j;
+void destroy_row(BoardCell **row, int length) {
+    int i;
+    for (i=0; i < length; i++) {
+        destroy_cell(row[i]);
+    }
 
+    free(row);
+}
+
+Board* create_board(int rows_in_block, int columns_in_block){
+    int i, dim;
+
+    Board *board = malloc(sizeof(Board));
+    validate_memory_allocation("create_board", board);
+
+    dim = rows_in_block * columns_in_block;
     board->num_of_rows_in_block = rows_in_block;
     board->num_of_columns_in_block = columns_in_block;
     board->dim = dim;
 
-    board->cells_arr = calloc(dim*dim, sizeof(game_cell));
-    validate_memory_allocation("init_empty_board", board->cells_arr);
+    BoardCell ***cells_arr = malloc(dim * sizeof(BoardCell**));
+    validate_memory_allocation("create_board", board);
 
     for (i=0; i < dim; i++) {
-        for (j=0; j < dim; j++) {
-            board->cells_arr[i][j].val = CLEAR;
-            board->cells_arr[i][j].fixed = false;
-            board->cells_arr[i][j].erroneous = false;
-        }
+        cells_arr[i] = create_row(dim);
     }
 
+    board->cells_arr = cells_arr;
+    return board;
 }
 
 void destroy_board(Board *board){
+    int i;
+    for (i=0; i < board->dim; i++) {
+        destroy_row(board->cells_arr[i], board->dim);
+    }
+
     free(board->cells_arr);
+    free(board);
 }
--- components/Board.h
+++ components/Board.h
@@ -9,28 +9,22 @@ typedef struct {
     int val;
     bool fixed;
     bool erroneous;
-} game_cell;
+} BoardCell;
 
 typedef struct {
     int dim;
     int num_of_rows_in_block;
     int num_of_columns_in_block;
-    game_cell cells_arr[][];
+    BoardCell ***cells_arr;
     bool solved;
 } Board;
 
-
-/* Alert of a failed memory allocation, and exit */
-void report_memory_allocation_error(char* function_name);
-
-/* Validate that a memory allocation was successful. If not, report failure */
-void validate_memory_allocation(char* function_name, void *p);
 
 /* Initialize the given board:
  * set the dimension fields.
  * set the cells values to CLEAR, which means that the board is empty.
  * */
-void init_empty_board(Board *board, int dim, int rows_in_block, int columns_in_block);
+Board* create_board(int rows_in_block, int columns_in_block);
 
 void destroy_board(Board *board);
 
