f6ec9d58df7891ec4109b3dea817bbd08c4e472a
Lior Sternberg <liors1@mail.tau.ac.il>
2019-08-12 14:50:03 +0300

    Start working on the printer

--- Printer.h
+++ Printer.h
@@ -1,4 +1,13 @@
 #ifndef FINAL_PROJECT_PRINTER_H
 #define FINAL_PROJECT_PRINTER_H
 
+#include "components/Board.h"
+
+/** Printer:
+ *   this module is responsible for printing the game board. */
+
+
+/* Prints the sudoku board to stdout. */
+void print_board(Board *board);
+
 #endif
