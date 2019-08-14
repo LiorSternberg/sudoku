cef6f259a4200068e7e49fc561b7d7f4af1d65b7
Lior Sternberg <liors1@mail.tau.ac.il>
2019-08-15 01:43:22 +0300

    typo

--- io/Parser.h
+++ io/Parser.h
@@ -8,7 +8,7 @@
  *   this module is responsible for parsing raw commands (text entered by the user)
  *   into structured Commands.
  *
- *  [Note: errors that are not parsing errors (e.g. not related to the type or number
+ *  [Note: errors that are not parsing errors (i.e. not related to the type or number
  *   of arguments) will not be recognized by the parser. Use the 'validate_command'
  *   function for more comprehensive validations.] */
 
