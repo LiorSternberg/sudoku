ae7d2bd9d200c16a00c538c7868f67fb93aba6c4
Tslil Chen <tslilchen@mail.tau.ac.il>
2019-08-23 17:38:54 +0300

    Added Stack files

---
+++ components/Stack.c
@@ -0,0 +1,5 @@
+//
+// Created by USER on 23/08/2019.
+//
+
+#include "Stack.h"
---
+++ components/Stack.h
@@ -0,0 +1,8 @@
+//
+// Created by USER on 23/08/2019.
+//
+
+#ifndef FINAL_PROJECT_STACK_H
+#define FINAL_PROJECT_STACK_H
+
+#endif //FINAL_PROJECT_STACK_H
