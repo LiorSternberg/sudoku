4975e4f125d318dadf5b059c075ff11316dce638
Tslil Chen <tslilchen@mail.tau.ac.il>
2019-08-23 18:07:10 +0300

    Added Stack structs: Parameters, StackNode, Stack

--- components/Stack.h
+++ components/Stack.h
@@ -5,4 +5,21 @@
 #ifndef FINAL_PROJECT_STACK_H
 #define FINAL_PROJECT_STACK_H
 
-#endif //FINAL_PROJECT_STACK_H
+typedef struct{
+    int i;
+    int j;
+    int val;
+} Parameters;
+
+typedef struct StackNode StackNode;
+
+struct StackNode{
+    Parameters params;
+    StackNode *next;
+};
+
+typedef struct{
+    StackNode *top;
+} Stack;
+
+#endif
