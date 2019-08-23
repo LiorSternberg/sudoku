f45ed55b33cf48c0b40b7b9b3077f9bcca16eb16
Tslil Chen <tslilchen@mail.tau.ac.il>
2019-08-23 18:51:37 +0300

    Added functions to Stack

--- components/Stack.c
+++ components/Stack.c
@@ -1,5 +1,53 @@
 //
 // Created by USER on 23/08/2019.
 //
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "Stack.h"
+
+Stack* create_stack(){
+    Stack *stack = (Stack)malloc(sizeof(Stack));
+    validate_memory_allocation("create_stack", stack);
+    stack->top = NULL;
+    return stack;
+}
+
+StackNode* create_stack_node(int i, int j, int val){
+    StackNode *stack_node = (StackNode)malloc(sizeof(StackNode));
+    validate_memory_allocation("create_stack_node", stack_node);
+    stack->top = NULL;
+    return stack;
+}
+
+void push(int i, int j, int val, Stack *stack){
+    StackNode *new_top = create_stack_node(i, j, val);
+    new_top->next = stack->top;
+    stack->top = new_top;
+}
+
+StackNode pop(Stack *stack){ /*Remember(for Tslil): free the node somewhere after using it*/
+    StackNode res_node = stack->top;
+    stack->top = stack->top->next;
+    return res_node;
+}
+
+StackNode top(Stack *stack){
+    return stack->top;
+}
+
+bool is_empty_stack(Stack *stack){
+    return (stack->top == NULL);
+}
+
+void destroy_stack_node(StackNode *node){
+    free(node);
+}
+
+void destroy_stack(Stack *stack){
+    StackNode *node;
+    while (stack->top != NULL){
+        node = pop(stack);
+        free(node);
+    }
+    free(stack);
+}
--- components/Stack.h
+++ components/Stack.h
@@ -5,6 +5,8 @@
 #ifndef FINAL_PROJECT_STACK_H
 #define FINAL_PROJECT_STACK_H
 
+#include <stdbool.h>
+
 typedef struct{
     int i;
     int j;
@@ -21,5 +23,21 @@ struct StackNode{
 typedef struct{
     StackNode *top;
 } Stack;
+
+Stack* create_stack();
+
+StackNode* create_stack_node(int i, int j, int val);
+
+void push(int i, int j, int val, Stack *stack);
+
+StackNode pop(Stack *stack);
+
+StackNode top(Stack *stack);
+
+bool is_empty_stack(Stack *stack);
+
+void destroy_stack_node(StackNode *node);
+
+void destroy_stack(Stack *stack);
 
 #endif
