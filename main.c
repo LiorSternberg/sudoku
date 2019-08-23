9d119472cb0ab75cf5b37f4232db28641029099d
Lior Sternberg <liors1@mail.tau.ac.il>
2019-08-23 15:53:53 +0300

    Edited the main (Added an example for Tslil)

--- main.c
+++ main.c
@@ -9,12 +9,14 @@
 int main() {
     Command *command;
     Game *game = create_game();
-    Error *error = create_error();
     announce_game_start();
-//    game->board = load_from_file("../test_files/boards/board_2_5_with_fixed.txt", error);
-//    save_to_file(game, "../test_files/boards/test.txt", error);
-//    announce_error(error);
-//    ../test_files/boards/board_2_2_almost_full.txt
+
+    /* Ty runnning:
+     *
+     * solve ../test_files/boards/board_2_5_with_fixed.txt
+     * solve ../test_files/boards/board_2_2_almost_full.txt
+     *
+     */
 
     while (true) {
         play_turn(game, command);
