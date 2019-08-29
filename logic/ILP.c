2e53b26ea6a62d32b911e336b42fba0a4fe672e5
Lior Sternberg <liors1@mail.tau.ac.il>
2019-08-29 16:28:38 +0300

    Added ILP functions outline

--- logic/ILP.c
+++ logic/ILP.c
@@ -1,1 +1,18 @@
 #include "ILP.h"
+
+#define UNUSED(x) (void)(x)
+
+bool solve(Board *board) {
+    /* TODO: actually implement this, and then remove the UNUSED and
+     * change return value to be meaningful :) */
+    UNUSED(board);
+    return true;
+};
+
+bool is_board_solvable(const Board *board) {
+    /* TODO: actually implement this, and then remove the UNUSED and
+     * change return value to be meaningful :) */
+    UNUSED(board);
+    return true;
+}
+
--- logic/ILP.h
+++ logic/ILP.h
@@ -1,4 +1,17 @@
+#include <stdbool.h>
+#include "../components/Board.h"
+
 #ifndef FINAL_PROJECT_ILP_H
 #define FINAL_PROJECT_ILP_H
+
+
+/* Solves the board using ILP. Returns true if the board was solved
+ * successfully, and false if it is not solvable. */
+bool solve(Board *board);
+
+/* Returns true is the board has a solution (validated using ILP), and false
+ * if it doesn't. This function does not change the board in any way. */
+bool is_board_solvable(const Board *board);
+
 
 #endif
