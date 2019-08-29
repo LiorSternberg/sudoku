fdcdeaa07030c92445e4d8352f8826d12f8105ed
Lior Sternberg <liors1@mail.tau.ac.il>
2019-08-29 16:21:13 +0300

    Added LP functions outline

--- logic/LP.c
+++ logic/LP.c
@@ -1,1 +1,22 @@
+#include <stdlib.h>
 #include "LP.h"
+#include "../MemoryError.h"
+
+#define UNUSED(x) (void)(x)
+
+
+void guess_solution(Board *board, int threshold) {
+    UNUSED(board);
+    UNUSED(threshold);
+};
+
+int* get_cell_guesses(Board *board, int row, int column) {
+    int *guesses = calloc(board->dim, sizeof(int));
+    validate_memory_allocation("create_stack", guesses);
+
+    /* TODO: actually implement this, and then remove the UNUSED :) */
+    UNUSED(row);
+    UNUSED(column);
+
+    return guesses;
+}
--- logic/LP.h
+++ logic/LP.h
@@ -1,4 +1,20 @@
+#include "../components/Board.h"
+
 #ifndef FINAL_PROJECT_LP_H
 #define FINAL_PROJECT_LP_H
+
+
+/* Guesses a solution for the given board using LP (not ILP!), and fills the
+ * board so that only guesses over the threshold are considered.
+ *
+ * Note: if more than one possible value is found, randomly chooses a value,
+ *       using the scores as weights for the random choice. */
+void guess_solution(Board *board, int threshold);
+
+/* Returns an array of scores that represents the possible values and their
+ * score for the given cell (size of the array is always the board dimension).
+ * For example, the array [0, 0.1, 0.33, 0, 0.2, ...] means that the value 2 has a
+ * score of 0.1, and the value 3 has a score of 0.33, and so on. */
+int* get_cell_guesses(Board *board, int row, int column);
 
 #endif
