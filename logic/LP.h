00f51e5acb0c3719d8e4c8f2a6885efbd7acf7f2
Tslil Chen <tslilchen@mail.tau.ac.il>
2019-08-31 22:31:39 +0300

    Added LPSSol and Variable Structs

--- logic/LP.h
+++ logic/LP.h
@@ -3,6 +3,25 @@
 #ifndef FINAL_PROJECT_LP_H
 #define FINAL_PROJECT_LP_H
 
+typedef struct {
+    int row;
+    int column;
+    int val;
+    int var_index;
+} Variable;
+
+/*
+ * LPSol struct holds the information received from "get_LP_solution".
+ * "map_by_cell" is an array of Lists (node's item is Variable), the index (i, j) represents the variables list of the cell (i,j).
+ * "sol" holds the solution values of the variables.
+ * "solved" is set to 1 if LP found a solution to the board, 0 if there is no solution, and -1 if the LP solver encoutered an error.
+ */
+typedef struct {
+    int dim;
+    List **map_by_cell;
+    double *sol;
+    int solved;
+} LPSol;
 
 /* Guesses a solution for the given board using LP (not ILP!), and fills the
  * board so that only guesses over the threshold are considered.
@@ -17,4 +36,26 @@ void guess_solution(Board *board, int threshold);
  * score of 0.1, and the value 3 has a score of 0.33, and so on. */
 int* get_cell_guesses(Board *board, int row, int column);
 
-#endif
+
+/*
+ * Solves the board using linear programming and returns a LPSol struct with the solution info.
+ * The parameter "integer" - determines whether the solver will use LP (0) or ILP (1).
+ */
+LPSol* get_LP_solution(Board *board, int integer);
+
+
+Variable* create_variable(int row, int column, int val, int var_index);
+
+/* Assigns a variable to (row, col) List in "map_by_cell" field with value val and stores it's index */
+void add_variable(LPSol *board_sol, int row, int column, int val, int index);
+
+/* Returns the "var_index" of the variable Xijv */
+int get_variable_index(LPSol *board_sol, i, j, v);
+
+void destroy_variable(Variable *var);
+
+LPSol* create_LP_sol(int dim);
+
+void destroy_LP_sol(LPSol *board_sol);
+
+void destroyGurobi(GRBenv *env, GRBmodel *model, int *ind, double *val, double *obj, char *vtype);
