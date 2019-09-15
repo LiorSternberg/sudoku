d1c75616096397fe13b76ebf6c83c5e5099728b3
Lior Sternberg <liors1@mail.tau.ac.il>
2019-09-16 00:21:08 +0300

    Documented validators and Serializer

--- io/Serializer.h
+++ io/Serializer.h
@@ -4,16 +4,27 @@
 #include "Error.h"
 #include "../components/Game.h"
 
-/* Load a board from file. Also validates the format is correct.
- * Additional validations such as the legality of the values themselves, and
- * that there are no collisions of fixed cells are to be done separately. */
+/**
+ * Load a board from file. Also validates the format is correct.
+ * In addition, performs basic validations of the legality of the values
+ * themselves, and asserts that there are no collisions of fixed cells.
+ * @param path: the path of the input file. Must be an existing file with data
+ * in the correct format of a sudoku board.
+ * @param error: the error to set in case there is a problem loading the file.
+ * @param mode: the current mode of the game.
+ * @return: the loaded board in case of success, and NULL in case of error.
+ */
 Board* load_from_file(char *path, Error *error, GameMode mode);
 
-
-/* Save a board to a file.
+/**
+ * Save a board to a file.
  * If the game is in Edit mode, all values are saved as fixed.
  * This function does not validate the board, this should be done before
- * calling it. */
+ * calling it.
+ * @param game: the game to save (save the game board, according to the game mode).
+ * @param path: the path of the output file.
+ * @param error: the error to set in case there is a problem saving the file.
+ */
 void save_to_file(Game *game, char *path, Error *error);
 
 #endif
--- io/validators.h
+++ io/validators.h
@@ -12,36 +12,140 @@
 
 /* Command-specific validators (used by the Parser at the configuration stage) */
 
+/**
+ * The custom validator for the solve command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void solve_validator(Command *command, Game *game);
 
+
+/**
+ * The custom validator for the edit command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void edit_validator(Command *command, Game *game);
 
+/**
+ * The custom validator for the mark_errors command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void mark_errors_validator(Command *command, Game *game);
 
+/**
+ * The custom validator for the set command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void set_validator(Command *command, Game *game);
 
+/**
+ * The custom validator for the validate command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void validate_validator(Command *command, Game *game);
 
+/**
+ * The custom validator for the guess command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void guess_validator(Command *command, Game *game);
 
+/**
+ * The custom validator for the generate command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void generate_validator(Command *command, Game *game);
 
+/**
+ * The custom validator for the undo command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void undo_validator(Command *command, Game *game);
 
+/**
+ * The custom validator for the redo command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void redo_validator(Command *command, Game *game);
 
+/**
+ * The custom validator for the save command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void save_validator(Command *command, Game *game);
 
+/**
+ * The custom validator for the hint command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void hint_validator(Command *command, Game *game);
 
+/**
+ * The custom validator for the guess_hint command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void guess_hint_validator(Command *command, Game *game);
 
+/**
+ * The custom validator for the num_solutions command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void num_solutions_validator(Command *command, Game *game);
 
+/**
+ * The custom validator for the autofill command. In case the command is found to
+ * be invalid (if the game-mode is illegal for this command for example), the
+ * command's Error is updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void autofill_validator(Command *command, Game *game);
 
-/* Validates the command - adds a relevant error message and error level
- * if the command is found to be invalid. */
+/**
+ * Validates the command - In case the command is found to be invalid (if the
+ * game-mode is illegal for this command for example), the command's Error is
+ * updated to reflect the issue.
+ * @param command: the Command to validate.
+ * @param game: the current game.
+ */
 void validate_command(Command *command, Game *game);
 
 #endif
