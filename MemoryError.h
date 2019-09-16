5c289d0c8327b6a63081827538241e7c802c83c4
Lior Sternberg <liors1@mail.tau.ac.il>
2019-09-16 12:55:00 +0300

    Documented memory error

--- MemoryError.h
+++ MemoryError.h
@@ -1,13 +1,19 @@
 #ifndef HW3_MEMORYERROR_H
 #define HW3_MEMORYERROR_H
 
-/* MemoryError:
+/** MemoryError:
  *
  * Handles failed dynamic memory allocations.
  * */
 
 
-/* Validate that a memory allocation was successful. If not, report failure */
+/**
+ * Validates that a memory allocation was successful. If not, reports failure,
+ * and exits with error code 1.
+ * @param function_name: the name of the function where the memory allocation
+ * was attempted.
+ * @param p: the pointer to the alleged allocated memory.
+ */
 void validate_memory_allocation(char *function_name, void *p);
 
 #endif
