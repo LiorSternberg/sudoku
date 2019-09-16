57831ca6ac18ff1b16e0f91bf5fec42523aa64e7
Lior Sternberg <liors1@mail.tau.ac.il>
2019-09-16 12:43:35 +0300

    Documented backtracking

--- logic/Backtracking.h
+++ logic/Backtracking.h
@@ -6,13 +6,30 @@
 
 #endif
 
-/* Removes the top from the stack and updates row, column and val accordingly */
+/**
+ * Removes the top from the stack and updates row, column and val accordingly
+ * @param stack: the stack to remove the top from.
+ * @param row: pointer to the row current value, to be updated.
+ * @param column: pointer to the column current value, to be updated.
+ * @param val: pointer to the val current value, to be updated.
+ */
 void backtrack_to_stack_top(Stack *stack, int *row, int *column, int *val);
 
-/* Updates row and column according to the next cell coordinates (assuming that (row, column) is not the last cell) */
+/**
+ * Updates row and column according to the next cell coordinates (assuming that
+ * (row, column) is not the last cell).
+ * @param row: pointer to the row current value, to be updated.
+ * @param column: pointer to the column current value, to be updated.
+ * @param N: the board dimension.
+ */
 void update_next_cell_coordinates(int *row, int *column, int N);
 
-/* Returns the number of solutions of the board, and -1 if there is no solution (meaning the board is erroneous)*/
+/**
+ * Returns the number of solutions of the board, and -1 if there is no solution
+ * (meaning the board is unsolvable).
+ * @param board: the board to get the number of solutions for.
+ * @return: the number of solutions for the given board.
+ */
 int get_num_of_solutions(const Board *board);
 
 
