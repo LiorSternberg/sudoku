b750293d79c9a73552b85d1bbffb34f6d687782c
Lior Sternberg <liors1@mail.tau.ac.il>
2019-09-16 11:49:31 +0300

    Documented command

--- io/Command.h
+++ io/Command.h
@@ -8,6 +8,7 @@
 /** Command:
  *   this module defines the structured commands and their configurations. */
 
+/* Number of arguments per command type */
 #define NO_ARGS 0
 #define SOLVE_ARGS 1
 #define EDIT_MIN_ARGS 0
@@ -20,6 +21,7 @@
 #define HINT_ARGS 2
 #define GUESS_HINT_ARGS 2
 
+/* Command format per command type */
 #define SOLVE_FORMAT "solve <path>"
 #define EDIT_FORMAT "edit [<path>]"
 #define MARK_ERRORS_FORMAT "mark_errors <set>"
@@ -38,10 +40,12 @@
 #define RESET_FORMAT "reset"
 #define EXIT_FORMAT "exit"
 
+/* Numeric arguments' limits */
 #define MIN_THRESHOLD 0
 #define MAX_THRESHOLD 1
 #define MIN_CELLS 1
 
+/* Allowed game modes per command type */
 #define SOLVE_MODES (solve_mode + edit_mode + init_mode)
 #define EDIT_MODES (solve_mode + edit_mode + init_mode)
 #define MARK_ERRORS_MODES (solve_mode)
@@ -60,14 +64,13 @@
 #define RESET_MODES (solve_mode + edit_mode)
 #define EXIT_MODES (solve_mode + edit_mode + init_mode)
 
-/* The possible types of commands (empty refers to a non-command that should be ignored) */
 
+/* The possible types of commands ('empty' refers to a non-command that should be ignored) */
 typedef enum { solve, edit, mark_errors, print_board, set, validate, guess, generate, undo, redo, save, hint,
     guess_hint, num_solutions, autofill, reset, exit_game, empty } CommandType;
 
 
-/* The different commands' data */
-
+/* The different commands' additional data */
 typedef struct {
     char* path;
 } SolveCommand, SaveCommand;
@@ -103,7 +106,6 @@ typedef struct {
 
 
 /* The command information such as the type of the command, and the command-specific data. */
-
 typedef struct Command_ {
     CommandType type;
     char* format;
@@ -126,17 +128,34 @@ typedef struct Command_ {
 } Command;
 
 
-/* Sets a command's error message and error level, and by doing so, invalidating it. */
+/**
+ * Sets a command's error message and error level, and by doing so, invalidating it.
+ * @param command: the command to invalidate.
+ * @param error_message: the error message to set for the error.
+ * @param error_level: the error level to set the error to.
+ * @param message_allocated: true if the error message was allocated dynamic memory
+ * and will need to be freed eventually, and false otherwise.
+ */
 void invalidate(Command *command, char *error_message, int error_level, bool message_allocated);
 
-/* Initialize a new command */
+/**
+ * Creates a new command.
+ * @return: the command that was created.
+ */
 Command* create_command();
 
-/* Free all the resources used by the command */
+/**
+ * Destroys the given command (frees all related memory).
+ * @param command: the command to destroy.
+ */
 void destroy_command(Command* command);
 
-
-/* Check if a command is valid */
+/**
+ * Checks if the command is valid.
+ * @param command: the command to check.
+ * @return: true if the command is valid and no error has been set, and false
+ * otherwise.
+ */
 bool is_valid(Command *command);
 
 #endif
