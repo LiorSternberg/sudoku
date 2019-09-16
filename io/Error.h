75cb2d0a06e30791074b2d89414bbbdae18e6d45
Lior Sternberg <liors1@mail.tau.ac.il>
2019-09-16 11:41:39 +0300

    Documented error

--- io/Error.h
+++ io/Error.h
@@ -3,7 +3,15 @@
 
 #include <stdbool.h>
 
-/* The possible error levels */
+/** Error:
+ *
+ * A structure that hold information about the command's error status and message.
+ * When updated, the level of the error is considered, so that the most critical
+ * error is always the one that is kept.
+ */
+
+
+/* The possible error levels, by order */
 
 typedef enum {
     invalid_command_length,
@@ -25,16 +33,29 @@ typedef struct  {
 } Error;
 
 
-/* Initialize a new Error.
- * There are two options:
- *   - an empty error
- *   - an error with a message and a matching error level */
-
+/**
+ * Creates a new empty error.
+ * @return: the new error that was created.
+ */
 Error* create_error();
 
+/**
+ * Set an error to a certain error level with an appropriate message.
+ * Note that if the error is already set to a lower level (more critical) then
+ * the current error message will be discarded, and the error won't change.
+ * @param error: the error to update.
+ * @param message: the error message to set to the error.
+ * @param level: the error level to set the error to.
+ * @param message_allocated: true if the message was allocated dynamic memory
+ * and will need to be freed eventually, and false otherwise.
+ */
 void set_error(Error *error, char *message, ErrorLevel level, bool message_allocated);
 
 /* Free all the resources used by the error */
+/**
+ * Destroys the given error (frees all related memory).
+ * @param error: the error to destroy.
+ */
 void destroy_error(Error* error);
 
 #endif
